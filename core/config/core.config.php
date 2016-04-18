67165f76fbd227d50848893030f252604361a8b3
iris lee <me@orzzz.me>
2016-04-18 21:47:42 +0800

    Update core.config.php

--- core/config/core.config.php
+++ core/config/core.config.php
@@ -5,7 +5,7 @@ $GLOBALS['config']['default_mod'] = 'default';
 $GLOBALS['config']['default_action'] = 'index';
 
 $GLOBALS['config']['site_name'] = 'LazyPHP';
-$GLOBALS['config']['on_sae'] = true;
+$GLOBALS['config']['on_sae'] = false;
 
 $GLOBALS['config']['cookie_time'] = 60*60*24;
 $GLOBALS['config']['cookie_path'] = '/';
